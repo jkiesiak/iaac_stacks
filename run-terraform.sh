0ebe87016ce267d8e8962f9ecdc4f4a1ab21a870
Joanna Kiesiak <joanna.kiesiak@bigindustries.be>
2024-02-22 00:31:39 +0100

    feat: run and delete terraform infrastructure

---
+++ run-terraform.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export AWS_PROFILE=
+echo $AWS_PROFILE
+
+export WORKSPACE=joa_test1
+#export AWS_DEFAULT_REGION=eu-west-1
+#echo $AWS_DEFAULT_REGION
+
+echo "-----------------------------"
+terraform -chdir=./terraform init
+terraform -chdir=./terraform workspace new $WORKSPACE
+terraform -chdir=./terraform workspace select $WORKSPACE
+terraform -chdir=./terraform fmt
+terraform -chdir=./terraform apply -auto-approve -var is_development=true
