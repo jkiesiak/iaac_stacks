5c783e769a4a336bd4c86ffd9be9016708220cae
Joanna Kiesiak <joanna.kiesiak@bigindustries.be>
2024-03-05 00:48:46 +0100

    feat: add egress rules

--- terraform/network.tf
+++ terraform/network.tf
@@ -48,7 +48,20 @@ resource "aws_security_group" "security_group" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  egress {
+    from_port        = 5432
+    to_port          = 5432
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
   tags = {
     Name = "security_group-${local.name_alias}"
   }
