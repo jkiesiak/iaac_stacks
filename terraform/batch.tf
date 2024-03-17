0cc60b575461b8311d4d9cdb3933f1b505d30b33
Joanna Kiesiak <joanna.kiesiak@bigindustries.be>
2024-03-17 15:40:00 +0100

    feat: add aws batch resources to spin up docker image

--- terraform/batch.tf
+++ terraform/batch.tf
@@ -2,28 +2,6 @@ resource "aws_ecs_cluster" "batch_cluster" {
   name = "batch-cluster-${local.name_alias}"
 }
 
-resource "aws_iam_role" "ecs_instance_role" {
-  name = "ecs-instance-role-${local.name_alias}"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Principal = {
-          Service = ["ec2.amazonaws.com", "ecs.amazonaws.com"]
-        },
-        Effect = "Allow",
-        Sid    = "",
-      },
-    ],
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_instance_role" {
-  role       = aws_iam_role.ecs_instance_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
-}
 resource "aws_iam_role" "aws_batch_service_role" {
   name = "aws-batch-service-role-${local.name_alias}"
 
@@ -33,91 +11,58 @@ resource "aws_iam_role" "aws_batch_service_role" {
       {
         Action = "sts:AssumeRole",
         Principal = {
-          Service = "batch.amazonaws.com",
-        },
+          "Service" : [
+            "ec2.amazonaws.com",
+            "ecr.amazonaws.com",
+            "ecs-tasks.amazonaws.com",
+            "batch.amazonaws.com"
+        ] },
         Effect = "Allow",
-        Sid    = "",
       },
     ],
   })
 }
 
-
-resource "aws_iam_instance_profile" "ecs_instance_profile" {
-  name = "ecs-instance-profile-${local.name_alias}"
-  role = aws_iam_role.ecs_instance_role.name
+resource "aws_iam_role_policy_attachment" "ecs_instance_role" {
+  role       = aws_iam_role.aws_batch_service_role.id
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
-
 
 resource "aws_iam_role_policy_attachment" "aws_batch_service_role" {
   role       = aws_iam_role.aws_batch_service_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBatchServiceRole"
 }
-resource "aws_iam_role_policy_attachment" "aws_batch_full" {
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   role       = aws_iam_role.aws_batch_service_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSBatchFullAccess"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+resource "aws_iam_role_policy_attachment" "cloudwatch_logs_policy_attachment" {
+  role       = aws_iam_role.aws_batch_service_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_read_policy_attachment" {
+  role       = aws_iam_role.aws_batch_service_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly" #AmazonEC2ContainerRegistryFullAccess
+}
 resource "aws_batch_compute_environment" "sample_compute_env" {
-  compute_environment_name = "sample-compute-environment-2-${local.name_alias}"
+  compute_environment_name = "sample-compute-environment-${local.name_alias}"
   compute_resources {
-    instance_role = aws_iam_instance_profile.ecs_instance_profile.arn
-    instance_type = ["m4.large"]
-    max_vcpus     = 16
-    min_vcpus     = 0
-    type          = "EC2"
-    security_group_ids = [
-      aws_security_group.security_group.id,
-    ]
-    subnets = [aws_subnet.subnet_public_v1.id, aws_subnet.subnet_public_v2.id]
+    type               = "FARGATE"
+    security_group_ids = [module.vpc.default_security_group_id]
+    subnets            = [for subnet in module.vpc.public_subnets : subnet]
+    max_vcpus          = 16
+
   }
+
   service_role = aws_iam_role.aws_batch_service_role.arn
-  type         = "MANAGED"
-  state        = "ENABLED"
+  depends_on   = [aws_iam_role_policy_attachment.aws_batch_service_role]
 
-}
+  type  = "MANAGED"
+  state = "ENABLED"
 
-resource "aws_iam_role" "aws_batch_execution_role" {
-  name = "aws-batch-execution-role-${local.name_alias}"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com",
-        },
-        Effect = "Allow",
-        Sid    = "",
-      },
-    ],
-  })
-}
-resource "aws_iam_policy" "batch_logs_policy" {
-  name        = "batch-logs-policy-${local.name_alias}"
-  path        = "/logs/${local.name_alias}/"
-  description = "IAM policy for logging from AWS Batch jobs"
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ],
-        Resource = "arn:aws:logs:*:*:*"
-      },
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "batch_execution_role_logs" {
-  role       = aws_iam_role.aws_batch_execution_role.name
-  policy_arn = aws_iam_policy.batch_logs_policy.arn
 }
 
 resource "aws_batch_job_queue" "sample_job_queue" {
@@ -125,57 +70,53 @@ resource "aws_batch_job_queue" "sample_job_queue" {
   priority = 1
   state    = "ENABLED"
 
-  compute_environments = [
-    aws_batch_compute_environment.sample_compute_env.arn,
-  ]
-}
-
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name               = "tf-test-batch-exec-role-${local.name_alias}"
-  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
-}
-
-data "aws_iam_policy_document" "assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  compute_environments = [aws_batch_compute_environment.sample_compute_env.arn]
 }
 
 resource "aws_batch_job_definition" "sample_job_definition" {
-  name = "sample-job-definition-3-${local.name_alias}"
-  type = "container"
-  #  platform_capabilities = ["FARGATE"]
+  name                  = "sample-job-definition-5-${local.name_alias}"
+  type                  = "container"
+  platform_capabilities = ["FARGATE"]
 
   container_properties = jsonencode({
-    image   = "${local.account_id}.dkr.ecr.${var.region_aws}.amazonaws.com/docker/${local.name_alias}:v0.1.2"
-    memory  = 512
-    vcpus   = 1
-    command = ["python", "hello_world.py"]
+    image   = "${aws_ecr_repository.my_repository.repository_url}:v0.1.4"
+    command = ["python", "lambda/hello_world.py"]
 
-    jobRoleArn = aws_iam_role.aws_batch_execution_role.arn
-    #    fargatePlatformConfiguration = {
-    #      platformVersion = "LATEST"
-    #    }
+    jobRoleArn = aws_iam_role.aws_batch_service_role.arn
+    fargatePlatformConfiguration = {
+      platformVersion = "LATEST"
+    }
+    networkConfiguration = {
+      "assignPublicIp" : "ENABLED"
+    }
+    resourceRequirements = [
+      {
+        type  = "VCPU",
+        value = "0.25"
+      },
+      {
+        type  = "MEMORY",
+        value = "512"
+      }
+    ]
 
     logConfiguration : {
       logDriver : "awslogs",
       options : {
-        awslogs-group : "/aws/batch/job",
+        awslogs-group : aws_cloudwatch_log_group.log_group.id
         awslogs-region : var.region_aws,
-        awslogs-stream-prefix : "${local.name_alias}"
+        awslogs-stream-prefix : local.name_alias
       }
     }
-    executionRoleArn = aws_iam_role.ecs_task_execution_role.arn
+    executionRoleArn = aws_iam_role.aws_batch_service_role.arn
 
   })
+
+  timeout {
+    attempt_duration_seconds = 60
+  }
+}
+
+resource "aws_cloudwatch_log_group" "log_group" {
+  name = "/aws/batch/${local.name_alias}"
 }
