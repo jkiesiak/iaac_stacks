a22bbcfb70d4a10378f8b5b0efa4997b8d945ab0
Joanna Kiesiak <joanna.kiesiak@bigindustries.be>
2024-05-13 11:11:05 +0200

    feat: add timeout 15sec

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -56,6 +56,8 @@ resource "aws_lambda_function" "terraform_lambda_func" {
   handler    = "lambda_handler.lambda_handler"
   runtime    = "python3.8"
   depends_on = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+  timeout          = 15
+
 
   environment {
     variables = {
