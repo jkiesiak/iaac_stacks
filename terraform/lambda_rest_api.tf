4a2a57c1349772ef94d883a2dae645c3e5b9a538
Joanna Kiesiak <joanna.kiesiak@bigindustries.be>
2025-04-18 20:40:16 +0200

    fixed: token auth to get data

--- terraform/lambda_rest_api.tf
+++ terraform/lambda_rest_api.tf
@@ -1,5 +1,5 @@
 resource "aws_lambda_function" "lambda_rest_api" {
-  function_name = "Lambda-Api-${local.name_alias}"
+  function_name = "lambda-rest-api-response-${local.name_alias}"
 
   filename         = "${path.module}/lambda_rest_api/.output/lambda_handler.zip"
   layers           = [aws_lambda_layer_version.python_pg8000_layer.arn, aws_lambda_layer_version.python_logging_layer.arn]
@@ -19,10 +19,15 @@ resource "aws_lambda_function" "lambda_rest_api" {
       DB_HOST         = aws_db_instance.rds.address
     }
   }
-
   depends_on = [
-    aws_iam_role_policy_attachment.attach_essential_policies_to_lambda_role
+    aws_iam_role_policy_attachment.attach_policy,
+    aws_cloudwatch_log_group.lambda_rest_api_logs
   ]
+}
+
+resource "aws_cloudwatch_log_group" "lambda_rest_api_logs" {
+  name              = "/aws/lambda/lambda-rest-api-response-${local.name_alias}"
+  retention_in_days = 14
 }
 
 
@@ -55,44 +60,33 @@ resource "aws_iam_role" "lambda_rest_api" {
 resource "aws_iam_policy" "lambda_rest_api_policy" {
   name = "lambda_rest_api_policy-${local.name_alias}"
 
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      # Allow access to Secrets Manager for retrieving secrets
-      {
-        Effect : "Allow",
-        Action : [
-          "secretsmanager:GetSecretValue"
-        ],
-        Resource : "arn:aws:secretsmanager:*:*:*"
-      },
-      # Allow Lambda to log to CloudWatch
-      {
-        Effect : "Allow",
-        Action : [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ],
-        Resource : "arn:aws:logs:*:*:*"
-      },
-      # (Optional) Allow access to RDS if Lambda needs it
-      {
-        Effect : "Allow",
-        Action : [
-          "rds:DescribeDBInstances",
-          "rds:Connect"
-        ],
-        Resource : "*"
-      },
-      # Allow Lambda to be invoked by API Gateway
-      {
-        Effect : "Allow",
-        Action : "lambda:InvokeFunction",
-        Resource : [aws_lambda_function.lambda_rest_api.arn, aws_lambda_function.lambda_token_authorizer.arn]
-      }
-    ]
-  })
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+          "logs:DescribeLogStreams",
+
+        "s3:GetObject",
+        "s3:PutObject",
+        "s3:ListBucket",
+        "s3:DeleteObject",
+        "secretsmanager:GetSecretValue",
+        "secretsmanager:DescribeSecret",
+        "rds:DescribeDBInstances",
+        "rds:Connect",
+        "lambda:InvokeFunction"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
 }
 
 # Attach the policy to the IAM role
@@ -102,22 +96,22 @@ resource "aws_iam_role_policy_attachment" "attach_policy" {
 }
 
 resource "aws_lambda_permission" "apigateway_lambda_invoke" {
-  statement_id  = "AllowAPIGatewayInvoke2"
+  statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.lambda_rest_api.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.rest_api.execution_arn}/*"
+  source_arn    = "${aws_api_gateway_rest_api.rest_api.execution_arn}/*/*"
 }
 
 resource "aws_lambda_function" "lambda_token_authorizer" {
+  function_name    = "lambda-token-authorizer-${local.name_alias}"
   filename         = "${path.module}/lambda_grant_token_access/.output/lambda_handler.zip"
   layers           = [aws_lambda_layer_version.python_pg8000_layer.arn, aws_lambda_layer_version.python_logging_layer.arn]
   source_code_hash = data.archive_file.zip_the_lambda_token_access.output_base64sha256
 
-  function_name = "lambda-token-authorizer-${local.name_alias}"
-  role          = aws_iam_role.lambda_rest_api.arn
-  handler       = "lambda_handler.lambda_handler"
-  runtime       = "python3.8"
+  role    = aws_iam_role.lambda_rest_api.arn
+  handler = "lambda_handler.lambda_handler"
+  runtime = "python3.8"
 
   environment {
     variables = {
@@ -131,33 +125,3 @@ data "archive_file" "zip_the_lambda_token_access" {
   source_dir  = "${path.module}/lambda_grant_token_access/src"
   output_path = "${path.module}/lambda_grant_token_access/.output/lambda_handler.zip"
 }
-
-resource "aws_lambda_permission" "apigateway_lambda_token_invoke" {
-  statement_id  = "AllowAPIGatewayInvoke-${local.name_alias}"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.lambda_token_authorizer.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.rest_api.execution_arn}/*"
-
-}
-
-data "aws_iam_policy_document" "lambda_logging_doc" {
-  statement {
-    effect = "Allow"
-    actions = [
-      "logs:CreateLogGroup",
-      "logs:CreateLogStream",
-      "logs:PutLogEvents",
-    ]
-    resources = ["arn:aws:logs:*:*:*"]
-  }
-}
-
-
-resource "aws_iam_policy" "lambda_logging_policy" {
-  name        = "lambda_logging_policy-${local.name_alias}"
-  path        = "/"
-  description = "IAM policy for logging from a lambda"
-  policy      = data.aws_iam_policy_document.lambda_logging_doc.json
-}
-
